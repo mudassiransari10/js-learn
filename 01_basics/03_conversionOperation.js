668e4dc303260f08ac3b93a23dabddaf44e0b3ff
Mudassir Ansari <65652074+mudassiransari10@users.noreply.github.com>
2023-12-20 12:45:13 +0000

    comparsion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,5 +22,40 @@ const isLoggedIn = 1
 
 const rank = 94208
 const rankString = String(rank)
-console.log(typeof rankString)
-console.log(rankString)
+// console.log(typeof rankString)
+// console.log(rankString)
+
+// ****************Operations****************
+
+const val = 3
+const negVal = -val
+// console.log(negVal)
+
+// console.log("2 + 2: ", 2 + 2)
+// console.log("2 - 2: ", 2 - 2)
+// console.log("2 * 2: ",2 * 2)
+// console.log("2 / 2: ", 2 / 2)
+// console.log("2 % 3: ", 2 % 3)
+// console.log("2 ** 2: ", 2 ** 2)
+
+let str1 = "Hello"
+let str2 = " Mudassir"
+let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(+true)
+// console.log(+"")
+
+let num1 = num2 = num3 = 2 + 2
+// console.table([num1, num2, num3])
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+
