bfe2432c52688689bafcc457d7280148ab459d54
Mudassir Ansari <65652074+mudassiransari10@users.noreply.github.com>
2023-12-25 13:48:10 +0000

    Dates

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,36 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate)
+// console.log(`myDate.toString(): ${myDate.toString()}`)
+// console.log(`myDate.toDateString(): ${myDate.toDateString()}`)
+// console.log(`myDate.toISOString(): ${myDate.toISOString()}`)
+// console.log(`myDate.toLocaleDateString(): ${myDate.toLocaleDateString()}`)
+// console.log(`myDate.toLocaleString(): ${myDate.toLocaleString()}`)
+// console.log(`myDate.toLocaleTimeString(): ${myDate.toLocaleTimeString()}`)
+// console.log(`myDate.toTimeString(): ${myDate.toTimeString()}`)
+// console.log(`myDate.toUTCString(): ${myDate.toUTCString()}`)
+// console.log(typeof myDate)
+
+// let currentDate1 = new Date(2023, 0, 23)
+// console.log(currentDate1.toDateString())
+// let currentDate2 = new Date(2023, 0, 23, 7, 15)
+// console.log(currentDate2.toLocaleString())
+// let currentDate3 = new Date("2023-12-25")
+// console.log(currentDate3.toLocaleString())
+let currentDate4 = new Date("12-25-2023")
+// console.log(currentDate4.toLocaleString())
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(currentDate4.getTime())
+// console.log(Math.floor(Date.now() / 1000))
+
+let newDate = new Date()
+// console.log(newDate)
+// console.log(`newDate.getMonth(): ${newDate.getMonth() + 1}`)
+// console.log(`newDate.getDay(): ${newDate.getDay()}`)
+
+// console.log(newDate.toLocaleString('default', {
+//     weekday: "long"
+// }))
