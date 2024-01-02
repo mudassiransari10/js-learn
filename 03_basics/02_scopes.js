25af85e2bbe6193a021387df0d41c921c32419b6
Mudassir Ansari <65652074+mudassiransari10@users.noreply.github.com>
2024-01-02 14:05:45 +0000

    scopes part2 and hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -2,12 +2,48 @@
 let a = 300
 
 
-if (true) {
-    let a = 10
-    const b = 20
-    console.log(`Inner: ${a}`)
-}
+// if (true) {
+//     let a = 10
+//     const b = 20
+//     console.log(`Inner: ${a}`)
+// }
 
-console.log(a)
+// console.log(a)
 // console.log(b)
 // console.log(c)
+
+function one() {
+    const username = "mudassir"
+    function two() {
+        const website = "youtube"
+        console.log(username)
+    }
+    // console.log(website)
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "mudassir"
+    if (username === "mudassir") {
+        const website = "youtube"
+        // console.log(`${username}  ${website}`)
+    }
+    // console.log(website)
+}
+// console.log(username)
+
+// ++++++++++++++++++++++++++++ Interesting ++++++++++++++++++++++++++++
+
+// console.log(addOne(5))
+function addOne(num) {
+    return num + 1
+}
+
+
+// addTwo(5)
+const addTwo = function(num) {
+    return num + 2
+}
+
