a52ed9bd78e3d246573fb48ed7d38fdcbfa9ad5d
Mudassir Ansari <65652074+mudassiransari10@users.noreply.github.com>
2024-01-04 10:33:55 +0000

    for in loop (Higher order loop)

---
+++ 05_iterations/four.js
@@ -0,0 +1,30 @@
+// for in
+
+
+const myObject = {
+    js: 'Javascript',
+    cpp: 'C++',
+    rb: 'Ruby',
+    swift: 'swift by apple'
+}
+
+// for (const key in myObject) {
+//     console.log(`${key} is short for ${myObject[key]}`)
+// }
+
+const programming = ["Js", "cpp", "c", "java"]
+
+
+// for (const key in programming) {
+//     console.log(programming[key])
+// }
+
+const map = new Map()
+map.set('IN', 'India')
+map.set('US', 'United States of America')
+map.set('Fr', 'France')
+map.set('IN', 'India')
+
+// for (const key in map) {
+//     console.log(key, ': ', 'value')
+// }
