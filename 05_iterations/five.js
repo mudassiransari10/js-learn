c62100e47609bae733dd6c483d4063f0c38b46ef
Mudassir Ansari <65652074+mudassiransari10@users.noreply.github.com>
2024-01-04 10:34:12 +0000

    forEach method of array (Higher order array)

---
+++ 05_iterations/five.js
@@ -0,0 +1,40 @@
+// forEach method of array
+
+const coding = ['js', 'ruby', 'java', 'cpp', 'python']
+
+// coding.forEach( function (item) {
+//     console.log(item)
+// })
+
+// coding.forEach( (item) => {
+//     console.log(item)
+// })
+
+// function printMe(item) {
+//     console.log(item)
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr)
+// })
+
+const myCoding = [
+    {
+        languageName: 'javascript',
+        languageExtension: 'js'
+    },
+    {
+        languageName: 'java',
+        languageExtension: 'java'
+    },
+    {
+        languageName: 'python',
+        languageExtension: 'py'
+    }
+]
+
+// myCoding.forEach( (item) => {
+//     console.log(item.languageName)
+// });
